31c3a83c39ec629e613f8f14c229d53e02b8bea1
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2025-06-17 09:23:51 +0530

    Update Prob2.java

--- Lab_2/Prob2.java
+++ Lab_2/Prob2.java
@@ -4,8 +4,7 @@ import java.util.*;
 
 class Prob2 {
 
-    static ArrayList<Integer> bfs(
-        ArrayList<ArrayList<Integer>> adj) {
+    static ArrayList<Integer> bfs(ArrayList<ArrayList<Integer>> adj) {
         int V = adj.size();
         int s = 0;
         ArrayList<Integer> res = new ArrayList<>();
@@ -31,15 +30,36 @@ class Prob2 {
     public static void main(String[] args) {
 
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        adj.add(new ArrayList<>(Arrays.asList(1, 2)));
-        adj.add(new ArrayList<>(Arrays.asList(0, 2, 3)));
-        adj.add(new ArrayList<>(Arrays.asList(0, 1, 4)));
-        adj.add(new ArrayList<>(Arrays.asList(1, 4)));
-        adj.add(new ArrayList<>(Arrays.asList(2, 3)));
+        // adj.add(new ArrayList<>(Arrays.asList(1, 2)));
+        // adj.add(new ArrayList<>(Arrays.asList(0, 2, 3)));
+        // adj.add(new ArrayList<>(Arrays.asList(0, 1, 4)));
+        // adj.add(new ArrayList<>(Arrays.asList(1, 4)));
+        // adj.add(new ArrayList<>(Arrays.asList(2, 3)));
+
+        Scanner sc = new Scanner(System.in);
+        int v;
+
+        System.out.println("Enter the number of vertices: ");
+        v = sc.nextInt();
+
+        for (int i = 0; i < v; i++) {
+            System.out.println("Enter the number of edges for vertex " + i + ": ");
+            int e = sc.nextInt();
+
+            ArrayList<Integer> edges = new ArrayList<>();
+            System.out.println("Enter the edges for vertex " + i + ": ");
+
+            for (int j = 0; j < e; j++) {
+                edges.add(sc.nextInt());
+            }
+            adj.add(edges);
+        }
 
         ArrayList<Integer> ans = bfs(adj);
         for (int i : ans) {
             System.out.print(i + " ");
         }
+
+        sc.close();
     }
 }
