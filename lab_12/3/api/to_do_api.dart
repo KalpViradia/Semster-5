e28055ea988bcb8f29b8f9cd7fd09e2dc12a51cd
Kalp Viradia <160616083+KalpViradia@users.noreply.github.com>
2025-07-18 08:53:01 +0530

    Update to_do_api.dart

--- lab_12/3/api/to_do_api.dart
+++ lab_12/3/api/to_do_api.dart
@@ -1,11 +1,17 @@
 import 'package:second/lab_12/3/utils/import_export.dart';
 
 class TodoApiService extends GetConnect implements ApiService {
-  final String baseUrl = 'https://66ed2084380821644cdb7fe9.mockapi.io/todos';
+  final String baseUrl = 'https://66ed2084380821644cdb7fe9.mockapi.io/';
+
+  @override
+  void onInit() {
+    httpClient.baseUrl = baseUrl;
+    allowAutoSignedCert = true;
+  }
 
   @override
   Future<List<Todo>> fetchTodos() async {
-    final res = await get(baseUrl);
+    final res = await get('todos');
     if (res.statusCode == 200) {
       return (res.body as List).map((e) => Todo.fromJson(e)).toList();
     } else {
@@ -15,16 +21,16 @@ class TodoApiService extends GetConnect implements ApiService {
 
   @override
   Future<void> addTodo(Todo todo) async {
-    await post(baseUrl, todo.toJson());
+    await post('todos', todo.toJson());
   }
 
   @override
   Future<void> updateTodo(String id, Todo todo) async {
-    await put('$baseUrl/$id', todo.toJson());
+    await put('todos/$id', todo.toJson());
   }
 
   @override
   Future<void> deleteTodo(String id) async {
-    await delete('$baseUrl/$id');
+    await delete('todos/$id');
   }
 }
